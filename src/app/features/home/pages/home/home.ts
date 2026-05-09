25653fc928630d1d56e5f74daff9912e68bffafe
Gerardo Cuomo <gerry.cuomo1994@gmail.com>
2026-05-09 15:04:08 +0200

    Implement live game search feature

--- src/app/features/home/pages/home/home.ts
+++ src/app/features/home/pages/home/home.ts
@@ -1,22 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 import { Game } from '../../../../models/game.model';
 import { GameService } from '../../../../services/game.service';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
 })
 export class Home implements OnInit {
   private readonly gameService = inject(GameService);
   private readonly cdr = inject(ChangeDetectorRef);
 
   games: Game[] = [];
+  searchTerm = '';
   isLoading = false;
   errorMessage = '';
 
@@ -32,16 +34,68 @@ export class Home implements OnInit {
       next: (response) => {
         this.games = response.results;
         this.isLoading = false;
-
         this.cdr.detectChanges();
       },
       error: (error) => {
         console.error(error);
         this.errorMessage = 'Errore durante il caricamento dei giochi.';
         this.isLoading = false;
-
         this.cdr.detectChanges();
-      }
+      },
+    });
+  }
+
+  searchGames(): void {
+    const search = this.searchTerm.trim();
+
+    if (!search) {
+      this.loadGames();
+      return;
+    }
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.gameService.searchGames(search).subscribe({
+      next: (response) => {
+        this.games = response.results;
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error(error);
+        this.errorMessage = 'Errore durante la ricerca dei giochi.';
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      },
+    });
+  }
+  onSearchChange(): void {
+    const search = this.searchTerm.trim();
+
+    this.games = [];
+    this.errorMessage = '';
+
+    if (!search) {
+      this.loadGames();
+      return;
+    }
+
+    this.isLoading = true;
+    this.cdr.detectChanges();
+
+    this.gameService.searchGames(search).subscribe({
+      next: (response) => {
+        this.games = response.results;
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error(error);
+        this.errorMessage = 'Errore durante la ricerca dei giochi.';
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      },
     });
   }
 }
