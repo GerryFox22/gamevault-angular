1161273ccc2cfe2dbd60cb420b851902c7f1488c
Gerardo Cuomo <gerry.cuomo1994@gmail.com>
2026-05-09 14:19:38 +0200

    Implement RAWG games homepage with manual change detection

--- src/app/features/home/pages/home/home.ts
+++ src/app/features/home/pages/home/home.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
+
+import { Game } from '../../../../models/game.model';
+import { GameService } from '../../../../services/game.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home implements OnInit {
+  private readonly gameService = inject(GameService);
+  private readonly cdr = inject(ChangeDetectorRef);
+
+  games: Game[] = [];
+  isLoading = false;
+  errorMessage = '';
+
+  ngOnInit(): void {
+    this.loadGames();
+  }
+
+  loadGames(): void {
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.gameService.getPopularGames().subscribe({
+      next: (response) => {
+        this.games = response.results;
+        this.isLoading = false;
+
+        this.cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error(error);
+        this.errorMessage = 'Errore durante il caricamento dei giochi.';
+        this.isLoading = false;
+
+        this.cdr.detectChanges();
+      }
+    });
+  }
+}
